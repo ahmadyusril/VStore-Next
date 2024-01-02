b587105b3e1fe15cb4b17c9771f42b9d647483cc
Yusril Ahmad <yusril7680@gmail.com>
2024-01-03 00:49:25 +0700

    first push

---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Container from "../container";
+import { Rubik_Glitch } from "next/font/google";
+
+const RubikGlitch = Rubik_Glitch({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+const Navbar = () => {
+  return (
+    <div
+      className="
+        sticky
+        top-0
+        w-full
+        bg-black
+        z-30
+        shadow-sm
+        "
+    >
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md-gap-0">
+            <Link
+              href="/"
+              className={`${RubikGlitch.className} font-bold text-2xl`}
+            >
+              Vanguard Store
+            </Link>
+            <div className="hidden md:block">Search</div>
+            <div className="flex items-center gap-8 md-gap-12">
+              <div>CartCount</div>
+              <div>UserMenu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
